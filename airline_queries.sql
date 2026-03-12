09731e28b06a7d9b7a2834fd483e3fc06ddd71df
ljwhite21 <ellipsis_music@yahoo.com>
2026-03-11 21:52:36 -0400

    Update airline_queries.sql

--- airline_queries.sql
+++ airline_queries.sql
@@ -9,7 +9,21 @@ ORDER BY Year;
 
 -- Query 2
 -- Highest average fares by airline
-
+WITH no_of_flights AS (
+  SELECT 
+    CARRIER_NAME,
+    COUNT(CARRIER_NAME) AS total_flights,
+    ROUND(AVG(`Average Market Fare _Current __`), 2) AS avg_fare
+  FROM `aff-average-air-fare-carrier.Air_Fare_Trends.AFF_carrier_data` 
+  GROUP BY CARRIER_NAME
+)
+SELECT 
+  total_flights,
+  CARRIER_NAME,
+  avg_fare
+FROM no_of_flights n
+WHERE n.total_flights > 29
+ORDER BY avg_fare DESC;
 
 -- Query 3
 -- Passenger volume by airline
