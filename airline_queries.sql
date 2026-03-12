6e286235b59bb743850f326838fbe690ec6ade0c
ljwhite21 <ellipsis_music@yahoo.com>
2026-03-11 21:57:24 -0400

    Update airline_queries.sql

--- airline_queries.sql
+++ airline_queries.sql
@@ -1,14 +1,26 @@
+=======================================================================
+-- U.S. Airline Fare Market Analysis
+-- Author: Levi White
+-- Dataset: Bureau of Transporation Statistics
+=======================================================================
+
+  
+=======================================================================
 -- Query 1
 -- Average airline fare by year
-
+=======================================================================
+  
 SELECT Year,
   ROUND(AVG(`Average Market Fare _Current __`), 2) AS avg_fare_by_year
 FROM `aff-average-air-fare-carrier.Air_Fare_Trends.AFF_carrier_data` 
 Group By Year
 ORDER BY Year;
 
+=======================================================================
 -- Query 2
 -- Highest average fares by airline
+=======================================================================
+  
 WITH no_of_flights AS (
   SELECT 
     CARRIER_NAME,
@@ -25,5 +37,7 @@ FROM no_of_flights n
 WHERE n.total_flights > 29
 ORDER BY avg_fare DESC;
 
+=======================================================================
 -- Query 3
 -- Passenger volume by airline
+=======================================================================
