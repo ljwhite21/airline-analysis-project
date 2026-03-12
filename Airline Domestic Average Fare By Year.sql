8470ed6d3ae37c7528d304f3a17f49a0a99e7fd1
ljwhite21 <ellipsis_music@yahoo.com>
2026-03-11 21:20:38 -0400

    Update Airline Domestic Average Fare By Year.sql

--- Airline Domestic Average Fare By Year.sql
+++ Airline Domestic Average Fare By Year.sql
@@ -2,4 +2,4 @@ SELECT Year,
   ROUND(AVG(`Average Market Fare _Current __`), 2) AS avg_fare_by_year
 FROM `aff-average-air-fare-carrier.Air_Fare_Trends.AFF_carrier_data` 
 Group By Year
-ORDER BY Year
+ORDER BY Year;
